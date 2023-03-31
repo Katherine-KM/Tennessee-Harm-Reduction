29b76595d406507e3390a66abe8f3346e0c0fcec
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2023-03-31 18:02:10 -0400

    refactor button, hero, and index based off global styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,30 +2,28 @@ import styled from 'styled-components';
 
 const ButtonComponent = styled.button`
 font-weight: 700;
-font-size: ${props => props.size === 'lg' ? '17px': '14px'};
+font-size: ${props => props.size === 'lg' ? 'var(--p-lg-fs)' :  'var(--p-sm-fs)'};
+// font-size: 20px; 
 line-height: 24px;
 text-align: center; 
 border-radius: 570px;
-max-width: ${props => props.size === 'lg' ? '280px' : props.size === 'xl' ? '500px': '150px'};
+max-width: ${props => props.size === 'lg' ? '320px' : props.size === 'xl' ? '500px': '150px'};
 padding: ${props => 
     props.size === 'lg' ? '10px 25px'
     : props.size === 'sm' ? '6px 21px'
     : '5px 10px'}; 
-color: ${props => 
-    props.bgColor === 'black' ? 'white'
-    : props.bgColor === 'white' ? 'black'
-    : 'white'}; 
-background-color: ${props => 
-    props.bgColor === 'black' ? 'black'
-    : props.bgColor === 'white' ? 'white'
-    : '#EB8C5B'};
+color: ${props => props.fontColor};
+background-color: ${props => props.bgColor};
 border: 2px solid ${props =>
     props.bgColor === 'white' ? 'black'
     : props.bgColor === 'black' ? 'black'
     : '#EB8C5B'};
+    &:hover { 
+      cursor: pointer;
+    }
 `
 
-const Button = ({ type, className, id, onClick, children, size, bgColor }) => {
+const Button = ({ type, className, id, onClick, children, size, bgColor, fontColor }) => {
   return (
     <ButtonComponent
       type={type ? type : "button"}
@@ -34,6 +32,7 @@ const Button = ({ type, className, id, onClick, children, size, bgColor }) => {
       onClick={onClick}
       size={size}
       bgColor={bgColor}
+      fontColor={fontColor}
     >
       {children}
     </ButtonComponent>
