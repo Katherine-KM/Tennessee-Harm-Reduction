76a403fe01dc1073e8a1b98c4e61322fb16de38e
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2023-04-13 21:48:40 -0400

    refactor top and bottom image container by componentizing the InnerImageContainerComponent

--- src/components/Service/BottomImagesContainer.jsx
+++ src/components/Service/BottomImagesContainer.jsx
@@ -1,112 +1,99 @@
 import styled from "styled-components";
 import Button from "../Button";
 import Image from "next/image";
+import InnerImageContainerComponent from "./InnerImageContainerComponent";
 
 const MainTopContainer = styled.div`
-    display:flex;
-    width: 100%;
-    min-height: 500px;
-    // background-color: blue;
-    // border: 6px blue solid;
+  display: flex;
+  width: 100%;
+  min-height: 500px;
+  // background-color: blue;
+  // border: 6px blue solid;
 
-    margin-bottom: 80px;
-    padding: 40px;
-    
-`
+  margin-bottom: 80px;
+  padding: 40px;
+`;
 
 const LeftHalfContainer = styled.div`
-    width: 50%;
-    min-height: 500px;
-    // background-color: red;
-`
+  width: 50%;
+  min-height: 500px;
+  // background-color: red;
+`;
 
 const RightHalfContainer = styled.div`
-    width: 50%;
-    min-height: 500px;
+  width: 50%;
+  min-height: 500px;
 
-    // background-color: green;
-`
+  // background-color: green;
+`;
 
 const InnerImageContainer = styled.div`
-    width: 90%;
-    min-height: 400px;
-    background-color: red;
-    margin: 90px auto;
-    position: relative;
-`
+  width: 90%;
+  min-height: 400px;
+  background-color: red;
+  margin: 90px auto;
+  position: relative;
+`;
 
 const TextContainer = styled.div`
-    width: 90%;
-    min-height: 360px;
-    background-color: green;
-    margin: 0 auto;
-    
-    position: absolute;
-    top: 300px;
-    left:30px;   
-`
+  width: 90%;
+  min-height: 360px;
+  background-color: green;
+  margin: 0 auto;
+
+  position: absolute;
+  top: 300px;
+  left: 30px;
+`;
 
 const InnerTextContainer = styled.div`
-    text-align:center;
-    padding: 50px;
-`
+  text-align: center;
+  padding: 50px;
+`;
 
 const BottomImagesContainer = ({
-    h1Text3,
-    paraText3,
-    buttonText1,
-    buttonText2,
+  h1Text3,
+  paraText3,
+  buttonText1,
+  buttonText2,
 
-    buttonText,
-    buttonSize,
-    buttonBgColor,
-    buttonFontColor,
+  buttonText,
+  buttonSize,
+  buttonBgColor,
+  buttonFontColor,
 
-    h1Text4,
-    paraText4,
-
+  h1Text4,
+  paraText4,
 }) => {
   return (
     <>
-  
-    <MainTopContainer>
+      <MainTopContainer>
         <LeftHalfContainer>
-            <InnerImageContainer>
-                <TextContainer>
-                    <InnerTextContainer>
-                    <h1 style={{fontSize: 'var(--h1-fs)',marginBottom: '20px'}}>{h1Text3}</h1>
-
-                    <p style={{fontSize: 'var(--p-md-fs)', marginBottom: '40px'}}>{paraText3}</p>
-                        <Button
-                       size={buttonSize}
-                       BgColor={buttonBgColor}
-                       FontColor={buttonFontColor}
-                        >fdsfsfds</Button>
-                    </InnerTextContainer>
-                </TextContainer>
-            </InnerImageContainer>
+          <InnerImageContainerComponent
+            h1Text="Treatment Program"
+            pText="Sending free NARCAN kits, syringes, and fentanyl test strips by mail within whole United state to improving the health and welfare of Tennesseans who use drugs."
+            buttonSize="xl"
+            buttonBgColor="orange"
+            buttonFontColor="white"
+            buttonText="Mat Treatment"
+            buttonText2="Other Harm Reduction Programs"
+          />
         </LeftHalfContainer>
 
         <RightHalfContainer>
-            <InnerImageContainer>
-                <TextContainer>
-                <InnerTextContainer>
-                    <h1 style={{fontSize: 'var(--h1-fs)',marginBottom: '20px'}}>{h1Text4}</h1>
-
-                    <p style={{fontSize: 'var(--p-md-fs)', marginBottom: '40px'}}>{paraText4}</p>
-                        <Button
-                       size={buttonSize}
-                       BgColor={buttonBgColor}
-                       FontColor={buttonFontColor}
-                        >fdsfsfds</Button>
-                    </InnerTextContainer>
-                </TextContainer>
-            </InnerImageContainer>
+          <InnerImageContainerComponent
+            h1Text="Information Center"
+            pText="Getting the most update information, including getting resources on safer drug use and knowing how to respond in the event of an overdose."
+            buttonSize="xl"
+            buttonBgColor="orange"
+            buttonFontColor="white"
+            buttonText="Become a Provider"
+            buttonText2="Common Questions"
+          />
         </RightHalfContainer>
-    </MainTopContainer>
-
+      </MainTopContainer>
     </>
-  )
-}
+  );
+};
 
-export default BottomImagesContainer
+export default BottomImagesContainer;
