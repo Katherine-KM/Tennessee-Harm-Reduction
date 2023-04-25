d8dbb3acc4e8761c800b88028d937949aafa277c
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2023-04-24 20:06:38 -0400

    refactor donation component to be mobile-first design and responsive

--- src/components/DonationComponent/DonationComponent.styles.js
+++ src/components/DonationComponent/DonationComponent.styles.js
@@ -5,28 +5,58 @@ import Image from "next/image";
 const DonationComponentContainer = styled.section`
     max-width: 90%;
     margin: auto; 
-    height: 49rem;
+    min-height: 49rem;
     display: flex;
     align-items: center; 
+    flex-direction: column-reverse; 
+
+    @media (min-width: 1060px) {
+        align-items: center;
+        flex-direction: row; 
+    }
 `
 
-const RightWrapper = styled.div`
-    width: 48%; 
-    height: 72%;
+const RightWrapper = styled.div` 
+    width: 100%;
+
+    @media (min-width: 800px) {
+        width: 70%;
+    }
+
+    @media (min-width: 1060px) {
+        min-height: inherit; 
+        width: 48%;
+    }
 `
+
+
 
 const LeftWrapper = styled.div`
-    width: 52%; 
+    text-align: center; 
+    width: 100%;
+
+    @media (min-width: 800px) {
+        width: 80%;
+    }
+
+    @media (min-width: 1060px) {
+        text-align: left; 
+        width: 52%;
+    }
 `
 
 const WrapperDetails = styled.div`
-
+    padding-right: 2rem; 
 `
 
 const WrapperPayment = styled.div`
     width: 98%; 
-    text-align: right; 
+    text-align: center; 
     position: relative; 
+
+    @media (min-width: 1060px) {
+        text-align: right; 
+    }
 `
 
 const StyledTitle = styled.h3`
@@ -62,12 +92,21 @@ const StyledButton = styled(ButtonComponent)`
     letter: -1.5%;
 `
 
-const ImageWrapper = styled.div`
+const ImageWrapper = styled.div` 
+    margin: 5.5rem auto; 
 `
 
 const StyledImage = styled(Image)`
     display: block; 
+    height: 100%; 
+    width: 100%; 
     margin: 0 auto; 
+
+    @media (min-width: 1060px) {
+        width: 70%;
+        max-height: 277px;
+        max-width: 393px;
+    }
 `
 
 export {DonationComponentContainer, RightWrapper, LeftWrapper, WrapperDetails, WrapperPayment, StyledP, StyledColoredTitle, StyledTitle, StyledInput, StyledSpan, StyledButton, ImageWrapper, StyledImage};
