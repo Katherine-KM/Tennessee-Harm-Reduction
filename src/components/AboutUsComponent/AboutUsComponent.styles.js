1b1742db1f830bfbd07cfc8035e289926503ed26
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2023-05-01 14:51:55 -0400

    add styling to the about us component

--- src/components/AboutUsComponent/AboutUsComponent.styles.js
+++ src/components/AboutUsComponent/AboutUsComponent.styles.js
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 
 const AboutUsContainer = styled.section`
-
-
+    width: 90%; 
+    margin: auto; 
+    padding: 2.5rem 0; 
+    min-height: fit-content; 
 `
 
 const TitleWrapper = styled.div`
-
+    display: flex;
+    align-items:center;
+    justify-content: space-between;
+    margin-bottom: 2rem;
 `
 
 const TitleRContainer = styled.div`
@@ -22,7 +27,7 @@ const DetailsWrapper = styled.div`
 `
 const StyledColoredTitle = styled.h3`
 color: var(--clr-primary-200);
-display: inline-block; 
+display: inline-block;  
 `
 const StyledTitle = styled.h3`
 display: inline-block; 
